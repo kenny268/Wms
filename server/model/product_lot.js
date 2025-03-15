374ddd047b05b64ce8b04a46eeeb51f6eef5138c
kenny268 <kentmutai5@gmail.com>
2025-03-15 16:29:17 +0300

    created a product lot table

---
+++ server/model/product_lot.js
@@ -0,0 +1,44 @@
+const ProductLot = sequelize.define('ProductLot', {
+    ProductLotID: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    ProductID: { // Foreign key
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'Products',
+            key: 'ProductID'
+        }
+    },
+    LotNumber: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    ExpirationDate: {
+        type: DataTypes.DATEONLY,
+    },
+    SystemTimestamp: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+    },
+}, {
+    uniqueKeys: {
+        uniqueProductLot: {
+            fields: ['ProductID', 'LotNumber']
+        }
+    }
+},{
+    timestamps: true,
+    tableName: 'product_lots',
+    indexes: [
+        {
+            unique: true,
+            fields: ['ProductID', 'LotNumber']
+        }
+    ]
+}
+);
+
+module.exports = ProductLot;
