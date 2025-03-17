171190dcacc100e6305d0235ba167686f417ab08
kenny268 <kentmutai5@gmail.com>
2025-03-17 20:14:53 +0300

    Created user model

--- server/models/user.js
+++ server/models/user.js
@@ -13,6 +13,63 @@ const User = sequelize.define('User', {
         unique: true,
         allowNull: false,
     },
+    FirstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    LastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    Email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+    },
+   
+    AddressID: { 
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'Addresses', 
+            key: 'AddressID'
+        }
+    },
+    Phone: {
+        type: DataTypes.STRING,
+    },
+    IsActive: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true,
+    },
+    IsDeleted: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+    },
+    CreatedByUserID: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'Users',
+            key: 'UserID'
+        }
+    },
+    ModifiedByUserID: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'Users',
+            key: 'UserID'
+        }
+    },
+    SystemTimestamp: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+    },
 },
 {
     timestamps: true,
