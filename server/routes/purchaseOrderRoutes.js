72a2207d82e642e737722365ef6a2814a7d473c9
kenny268 <kentmutai5@gmail.com>
2025-03-19 20:09:55 +0300

    Modified purchased order routes

--- server/routes/purchaseOrderRoutes.js
+++ server/routes/purchaseOrderRoutes.js
@@ -20,19 +20,20 @@ const {
     
  } = require('../validations/purchaseOrderValidation');
 const { createPurchaseOrder, getAllPurchaseOrders, getPurchaseOrderById, updatePurchaseOrder, deletePurchaseOrder } = require('../controllers/purchaseOrderController');
+const purchaseOrder = require('../controllers/purchaseOrderController');
 
 
 // Routes with associated controller functions and validation
-router.post('/api/purchase-orders', authenticateToken, authorizeRole(['purchaser', 'admin']), createPurchaseOrderValidationRules, createPurchaseOrder);
-router.get('/api/purchase-orders', authenticateToken, authorizeRole(['viewer', 'purchaser', 'admin']), getAllPurchaseOrders);
-router.get('/api/purchase-orders/:id', authenticateToken, authorizeRole(['viewer', 'purchaser', 'admin']), getPurchaseOrderByIdValidationRules, getPurchaseOrderById);
-router.put('/api/purchase-orders/:id', authenticateToken, authorizeRole(['purchaser', 'admin']), updatePurchaseOrderValidationRules, updatePurchaseOrder);
-router.delete('/api/purchase-orders/:id', authenticateToken, authorizeRole(['admin']), deletePurchaseOrderValidationRules, deletePurchaseOrder);
+router.post('/api/purchase-orders', authenticateToken, authorizeRole(['purchaser', 'admin']), createPurchaseOrderValidationRules, purchaseOrder.createPurchaseOrder);
+router.get('/api/purchase-orders', authenticateToken, authorizeRole(['viewer', 'purchaser', 'admin']), purchaseOrder.getAllPurchaseOrders);
+router.get('/api/purchase-orders/:id', authenticateToken, authorizeRole(['viewer', 'purchaser', 'admin']), getPurchaseOrderByIdValidationRules, purchaseOrder.getPurchaseOrderById);
+router.put('/api/purchase-orders/:id', authenticateToken, authorizeRole(['purchaser', 'admin']), updatePurchaseOrderValidationRules, purchaseOrder.updatePurchaseOrder);
+router.delete('/api/purchase-orders/:id', authenticateToken, authorizeRole(['admin']), deletePurchaseOrderValidationRules, purchaseOrder.deletePurchaseOrder);
 
-router.post('/api/purchase-orders/:purchaseOrderId/line-items', authenticateToken, authorizeRole(['purchaser', 'admin']), addLineItemToPurchaseOrderValidationRules, addLineItemToPurchaseOrder);
-router.get('/api/purchase-orders/:purchaseOrderId/line-items', authenticateToken, authorizeRole(['viewer', 'purchaser', 'admin']), getPurchaseOrderLineItemsValidationRules, getAllPurchaseOrderLineItems);
-router.get('/api/purchase-orders/line-items/:lineItemId', authenticateToken, authorizeRole(['viewer', 'purchaser', 'admin']), getPurchaseOrderLineItemByIdValidationRules, getPurchaseOrderLineItemById);
-router.put('/api/purchase-orders/line-items/:lineItemId', authenticateToken, authorizeRole(['purchaser', 'admin']), updatePurchaseOrderLineItemValidationRules, updatePurchaseOrderLineItem);
-router.delete('/api/purchase-orders/line-items/:lineItemId', authenticateToken, authorizeRole(['purchaser', 'admin']), deletePurchaseOrderLineItemValidationRules, deletePurchaseOrderLineItem);
+router.post('/api/purchase-orders/:purchaseOrderId/line-items', authenticateToken, authorizeRole(['purchaser', 'admin']), addLineItemToPurchaseOrderValidationRules, purchaseOrder.addLineItemToPurchaseOrder);
+router.get('/api/purchase-orders/:purchaseOrderId/line-items', authenticateToken, authorizeRole(['viewer', 'purchaser', 'admin']), getPurchaseOrderLineItemsValidationRules, purchaseOrder.getAllPurchaseOrderLineItems);
+router.get('/api/purchase-orders/line-items/:lineItemId', authenticateToken, authorizeRole(['viewer', 'purchaser', 'admin']), getPurchaseOrderLineItemByIdValidationRules, purchaseOrder.getPurchaseOrderLineItemById);
+router.put('/api/purchase-orders/line-items/:lineItemId', authenticateToken, authorizeRole(['purchaser', 'admin']), updatePurchaseOrderLineItemValidationRules, purchaseOrder.updatePurchaseOrderLineItem);
+router.delete('/api/purchase-orders/line-items/:lineItemId', authenticateToken, authorizeRole(['purchaser', 'admin']), deletePurchaseOrderLineItemValidationRules, purchaseOrder.deletePurchaseOrderLineItem);
 
 module.exports = router;
